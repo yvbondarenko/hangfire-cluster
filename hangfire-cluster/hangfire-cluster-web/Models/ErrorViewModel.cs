a3270d3f379bc7bfed6d815daeb844eaa3a4b630
Yuriy Bondarenko <yv.bondarenko@gmail.com>
2021-01-08 12:53:03 +0200

    init

---
+++ hangfire-cluster/hangfire-cluster-web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace hangfire_cluster_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
