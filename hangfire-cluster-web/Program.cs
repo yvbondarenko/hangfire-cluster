fdd5bc77cf334dd66fad3a485b7753b79ae05b20
Yuriy Bondarenko <yv.bondarenko@gmail.com>
2021-01-08 13:29:17 +0200

    Instert record to table every minute

--- hangfire-cluster-web/Program.cs
+++ hangfire-cluster-web/Program.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hangfire;
+using Hangfire.SqlServer;
 
 namespace hangfire_cluster_web
 {
--- hangfire-cluster-web/Startup.cs
+++ hangfire-cluster-web/Startup.cs
@@ -5,8 +5,11 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Hangfire;
+using Hangfire.SqlServer;
 
 namespace hangfire_cluster_web
 {
@@ -22,6 +25,21 @@ namespace hangfire_cluster_web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHangfire(configuration => configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseSqlServerStorage(Configuration.GetConnectionString("HangfireConnection"), new SqlServerStorageOptions
+                {
+                    CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                    SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                    QueuePollInterval = TimeSpan.Zero,
+                    UseRecommendedIsolationLevel = true,
+                    DisableGlobalLocks = true
+                }));
+
+            // Add the processing server as IHostedService
+            services.AddHangfireServer();
             services.AddControllersWithViews();
         }
 
@@ -37,7 +55,9 @@ namespace hangfire_cluster_web
                 app.UseExceptionHandler("/Home/Error");
             }
             app.UseStaticFiles();
-
+            app.UseHangfireDashboard();
+            RecurringJob.AddOrUpdate(() => InsertToDb(), Cron.Minutely);
+            
             app.UseRouting();
 
             app.UseAuthorization();
@@ -47,7 +67,24 @@ namespace hangfire_cluster_web
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapHangfireDashboard();
             });
+        }
+
+        public void InsertToDb()
+        {
+            string connectionString = @"Server=192.168.163.130\SCOM;Database=Hangfire-App-Cluster;User Id=sa;Password=P@ssword;";
+            string sqlExpression = "INSERT INTO [dbo].[Test] ([Datetime] ,[Host] ,[Message]) VALUES (@d, @h, @m)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                command.Parameters.Add(new SqlParameter("@d", DateTime.Now));
+                command.Parameters.Add(new SqlParameter("@h", Environment.MachineName));
+                command.Parameters.Add(new SqlParameter("@m", Guid.NewGuid().ToString()));
+                int number = command.ExecuteNonQuery();
+            }
         }
     }
 }
